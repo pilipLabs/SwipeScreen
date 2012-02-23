7584ddac910f0e0968dd02705b90ab23d7c7b404
Philippe Roussille <proussille@gmail.com>
2012-02-23 10:06:06 +0100

    First commit - android create

---
+++ src/net/pilip/labs/panscreen/PanScreen.java
@@ -0,0 +1,15 @@
+package net.pilip.labs.panscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PanScreen extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
